6de9b52a6f5d7ed71f5d18ed36c2762b45c61b94
Albert-7317 <103575236+Albert-7317@users.noreply.github.com>
2022-06-21 09:53:19 +0000

    Update catchBot.py

--- catchBot.py
+++ catchBot.py
@@ -16,15 +16,43 @@ def checkPkmn(pokemon):
     if pytesseract.image_to_string('pokemonName.png') == pokemon:
         pag.click(347, 626)
 
+def battleToDefeat():
+    screen = pag.size()
+    pag.click(347, 626)
+    time.sleep(3)
+    pag.scroll(-screen[1])
+    pag.click(573, 567)
+    time.sleep(3)
+    for j in range(0, 5):
+        pag.scroll(-screen[1])
+        pag.click(441, 360)
+        time.sleep(3)
+        pag.scroll(-screen[1])
+        y = 360
+        for i in range(0, 10):
+            pag.click(441, y)
+            time.sleep(0.5)
+            y += 21
+        pag.click(432, 618)
+    pag.click(500, 377)
+    time.sleep(3)
+
+
 while True:
     time.sleep(3)
     #checkPkmn('ekans')
-    print(pag.position())
+    #print(pag.position())
+    #battleToDefeat()
+    #
     for i in range(0, 3):
         time.sleep(2)
         move('left')
-    #   checkPkmn()
+        if random.randint(0, 2) == 0:
+            battleToDefeat()
+    #    checkPkmn()
         time.sleep(2)
         move('right')
+        if random.randint(0, 2) == 0:
+            battleToDefeat()
     #    checkPkmn()
     break
